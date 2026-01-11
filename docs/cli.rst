c28df82d5773745286d61bc20ae5e2815c1473fa
svalouch <svalouch@valouch.com>
2026-01-11 18:16:38 +0100

    fixthedocs

--- docs/cli.rst
+++ docs/cli.rst
@@ -10,11 +10,9 @@ The library comes with a CLI tool called ``rctclient`` that offers some useful s
 The tool is only installed if the `click <https://click.palletsprojects.com/>`_ module's present. If installing from
 `pip`, the requirement can be pulled in by specifying ``rctclient[cli]``.
 
-For certain parameters, the tool supports shell completion. Depending on your version of *Click*, there are different
-ways to enable the completion:
+For certain parameters, the tool supports shell completion, which you can activate for your current terminal::
 
-* ``<  8.0.0``: ``eval "$(_RCTCLIENT_COMPLETE=source_bash rctclient)"`` (e.g. Debian 11, Ubuntu 20.04)
-* ``>= 8.0.0``: ``eval "$(_RCTCLIENT_COMPLETE=bash_source rctclient)"`` (e.g. Arch, Fedora 35+, Gentoo)
+   eval "$(_RCTCLIENT_COMPLETE=bash_source rctclient)"
 
 Read more about this at the `click documentation <https://click.palletsprojects.com/en/7.x/bashcomplete/#activation>`_.
 
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
@@ -20,52 +19,36 @@ if on_rtd:
     subprocess.run('./generate_registry.py')
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'rctclient'
-copyright = '2020-2021, Peter Oberhofer, 2020-2023 Stefan Valouch'
+copyright = '2020-2021, Peter Oberhofer, 2020-2026 Stefan Valouch'
 author = 'Stefan Valouch'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.5'
 
-
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    'recommonmark',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx_autodoc_typehints',
-    'sphinx_click.ext',
-    'recommonmark',
+    'sphinx_click',
+    'sphinx_rtd_theme',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# Disable automatic quotes, which converts '--' to '—' (among others) which
-# breaks the CLI examples where the source is a python docstring and can't be
-# changed without interfering with CLI --help output.
 smartquotes = False
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 # -- Options for todo extension ----------------------------------------------
